4828f1f10783989a9e7f5e784539a717cdfa1218
Sai H <saih@Hitheshs-MacBook-Air.local>
2025-07-15 10:55:15 +0530

    Adding Error Handler

---
+++ src/handlers/errorHandler.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.error('Error occurred:', err.stack || err.message);
+
+  res.status(500).json({ error: err.message || 'Internal Server Error' });
+};
