7e0cfe1c1f5f424dfe85a93360a5bf4f333bcebc
Sai H <saih@Hitheshs-MacBook-Air.local>
2025-07-22 20:08:27 +0530

    Configure Swagger for API documentation

---
+++ src/utils/swagger.ts
@@ -0,0 +1,51 @@
+import { version } from '../../package.json';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express, Request, Response } from 'express';
+
+const options: swaggerJsDoc.Options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Inventory Management API',
+      version: version,
+      description: 'API documentation for the Inventory Management system',
+    },
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        BearerAuth: [],
+      },
+    ],
+    servers: [
+      {
+        url: 'http://localhost:4000/api/v1',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+function swaggerDocs(app: Express) {
+  // Swagger page
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  // Docs in JSON format
+  app.get('/api-docs.json', (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+  console.log(`Swagger docs available at http://localhost:4000/api-docs`);
+}
+
+export default swaggerDocs;
